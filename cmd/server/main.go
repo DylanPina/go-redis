f5118e67ea2d24d829a4d000d952fc3ba4467e1f
Dylan Pina <dylansp.dev@gmail.com>
2025-05-22 19:17:48 -0500

    Added PING command

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	port := flag.Int("port", 6379, "Port that the DNS server to listen on (default: 6379)")
+	flag.Parse()
+
+	l, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(*port))
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Fprintf(os.Stderr, "Failed to bind to port %d: %s\n", *port, err.Error())
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error accepting connection: %s", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading from connection: %s", err.Error())
+		return
+	}
+
+	request := string(buf[:n])
+	fmt.Printf("Received data: %s\n", request)
+
+	if strings.TrimSpace(request) != "PING" {
+		fmt.Fprintf(os.Stderr, "Expected PING, got: %s", request)
+		conn.Write([]byte("-ERR unknown command\r\n"))
+		return
+	}
+
+	conn.Write([]byte("+PONG\r\n"))
 }
---
+++ cmd/server/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+	"strconv"
+	"testing"
+)
+
+var TEST_PORT = flag.Int("port", 6379, "Port that the DNS server to listen on (default: 6379)")
+
+// TestMain is the entry point for the test suite
+func TestMain(m *testing.M) {
+	flag.Parse()
+	os.Exit(m.Run())
+}
+
+// TestPing tests the ping command
+func TestPong(t *testing.T) {
+	// Simulate a connection to the server
+	conn, err := net.Dial("tcp", "localhost:"+strconv.Itoa(*TEST_PORT))
+	if err != nil {
+		t.Fatalf("Failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	// Send a PING command
+	_, err = conn.Write([]byte("PING\r\n"))
+	if err != nil {
+		t.Fatalf("Failed to send PING command: %v", err)
+	}
+
+	// Listen for PONG response
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		t.Fatalf("Error reading from connection: %v", err)
+	}
+
+	if string(buf[:n]) != "PONG\r\n" {
+		t.Fatalf("Expected PONG, got: %s", string(buf[:n]))
+	}
+}
